5f7dbbe97d718d6c46e581c14ee4daf2cc6bd742
Safe Code Art <124263950+safecodeart14@users.noreply.github.com>
2023-04-12 23:07:57 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import bootstrapvue from 'bootstrap-vue/dist/bootstrap-vue.css';
+//import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+//import bootstrapvue from 'bootstrap-vue/dist/bootstrap-vue.css';
 new Vue({
-  bootstrap,
-  bootstrapvue,
+  //bootstrap,
+  //bootstrapvue,
   render: h => h(App)
 }).$mount("#app");
