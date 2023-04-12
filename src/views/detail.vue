db433d4fba10732ed09b4728ded33f11aabc5180
princysneha <37900772+princysneha@users.noreply.github.com>
2023-04-12 19:33:06 +0530

    fix

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,45 +1,64 @@
 <template>
-  <div v-if="this.selectedUser">
-    <span data-v-c43a1b98="">
-      <avatar
-        :first_name="this.selectedUser.first_name"
-        :last_name="this.selectedUser.last_name"
-        :profile="this.selectedUser.profile_pic"
-      ></avatar>
-      
-    </span> 
-    <span>{{ this.selectedUser.first_name }} {{ this.selectedUser.last_name }}</span>
-    <p>Username: {{ this.selectedUser.username }}</p>
-    <p>Address: {{ this.selectedUser.street_address }}
-      {{this.selectedUser.city}}
-    </p>
-    <p>{{this.selectedUser.bio}}</p>
-    <div class="footer">
-    <button type="button" class="btn btn-danger" @click="removeEmployee(this.selectedUser.id)">Delete</button>
-    <button type="button" class="btn btn-primary">Edit</button>
+  <div>
+    <div v-if="this.selectedUser && hideDelete">
+      <span data-v-c43a1b98="">
+        <avatar
+          :first_name="this.selectedUser.first_name"
+          :last_name="this.selectedUser.last_name"
+          :profile="this.selectedUser.profile_pic"
+        ></avatar>
+      </span>
+      <span
+        >{{ this.selectedUser.first_name }}
+        {{ this.selectedUser.last_name }}</span
+      >
+      <p>Username: {{ this.selectedUser.username }}</p>
+      <p>
+        Address: {{ this.selectedUser.street_address }}
+        {{ this.selectedUser.city }}
+      </p>
+      <p>{{ this.selectedUser.bio }}</p>
+      <div class="footer" v-if="hideDelete">
+        <button type="button" class="btn btn-danger" @click="removeEmployee">
+          Delete
+        </button>
+        <button type="button" class="btn btn-primary" @click="updateEmployee">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div>
+      <edit v-if="showForm" :user="selectedUser"></edit>
     </div>
   </div>
- 
 </template>
 
 <script>
-import avatar from '../common/avatar.vue';
+import avatar from "../common/avatar.vue";
+import edit from "./edit.vue";
 export default {
   name: "detail",
   props: {
-   selectedUser:{
-    default:[]
-   }
+    selectedUser: {
+      default: []
+    }
   },
-  components: {avatar
-  },
+  components: { avatar, edit },
+  data() {
+    return {
+      showForm: false,
 
+      hideDelete: true
+    };
+  },
   methods: {
-    parseMarkdown(content) {
-      return marked(content);
+    removeEmployee() {
+      this.$emit("clicked", this.selectedUser.id);
     },
-    removeEmployee(id){
-      
+    updateEmployee() {
+      console.log("emittttt", this.selectedUser.id);
+      this.showForm = true;
+      this.hideDelete = false;
     }
   }
 };
---
+++ src/views/edit.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <span>
+      <avatar
+        :first_name="this.user.first_name"
+        :last_name="this.user.last_name"
+        :profile="this.user.profile_pic"
+      ></avatar>
+    </span>
+    <div class="row">
+      <form>
+        <div class="form-group">
+          <label>First Name:</label>
+
+          <input type="text" class="form-control" v-model="first_name" />
+        </div>
+        <div class="form-group">
+          <label>Last Name:</label>
+
+          <input type="text" class="form-control" v-model="last_name" />
+        </div>
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="email" class="form-control" v-model="email" />
+        </div>
+        <button @click.prevent="save">Save</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+import avatar from "../common/avatar.vue";
+export default {
+  name: "edit",
+  components: {
+    avatar
+  },
+  props: {
+    user: {
+      default: []
+    }
+  },
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: ""
+    };
+  },
+  methods: {
+    save() {
+      console.log("formm", this.first_name);
+    }
+  }
+};
+</script>
